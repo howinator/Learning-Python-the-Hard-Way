3d424bb0102b39337651991924b3bf8da36e71bb
Paul (Howie) Benefiel <hben592@gmail.com>
2014-05-19 14:41:11 -0500

    Here I turned ex33 into a function that can be imported with the upper limist of the list changed into a variables wich is taken as the argument of the function.

--- ex33.py
+++ ex33.py
@@ -1,15 +1,17 @@
-i = 0
-numbers = []
+def append_loop(top_end):
 
-while i < 6:
-  print "At the top i is %d" % i
-  numbers.append(i)
+  i = 0
+  numbers = []
 
-  i = i + 1
-  print "Numbers now: ", numbers
-  print "At the bottom i is %d" % i
+  while i < top_end:
+    print "At the top i is %d" % i
+    numbers.append(i)
 
-print "The numbers: "
+    i = i + 1
+    print "Numbers now: ", numbers
+    print "At the bottom i is %d" % i
 
-for num in numbers:
-  print num
+  print "The numbers: "
+
+  for num in numbers:
+    print num
